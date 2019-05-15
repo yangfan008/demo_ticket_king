4877de5c321eac3f41c331ee77336c060b0838bd
yangfan008 <2101344239>
2019-05-15 11:46:48 +0800

    Initial commit

---
+++ src/main/java/com/ticket/demo_ticket_king/DemoTicketKingApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.demo_ticket_king;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTicketKingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTicketKingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticket/demo_ticket_king/DemoTicketKingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ticket.demo_ticket_king;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoTicketKingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
